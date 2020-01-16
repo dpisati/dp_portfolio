91056980a2f4be9d7cc32089fd68aaf9ddfccd43
Daniel Pisati <dpisati@gmail.com>
2020-01-16 21:39:10 +1300

    go_top_button

--- script.js
+++ script.js
@@ -65,3 +65,28 @@ function isElementInViewport(el) {
         (window.innerHeight || document.documentElement.clientHeight))
   );
 }
+
+//Get the button
+var mybutton = document.getElementById("gotop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
