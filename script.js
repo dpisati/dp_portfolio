d17273b2b1cefc1d0a390446ee77f08fcb5bf8f6
Daniel Pisati <dpisati@gmail.com>
2020-01-16 21:59:06 +1300

    btn-top-fixed

--- script.js
+++ script.js
@@ -84,9 +84,3 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
